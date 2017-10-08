423d02e3ac791a6f44ecd22339c9b0d499cbd2f8
HongXiang <hx.hongxiang@gmail.com>
2017-10-08 20:26:47 +0800

    init commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pygate',
+      version='0.1',
+      description='A simplified python interface for Gate.',
+      url='https://github.com/Hong-Xiang/pygate',
+      author='Hong Xiang',
+      author_email='hx.hongxiang@gmail.com',
+      license='MIT',
+      packages=[],
+      install_requires=['fs'],
+      zip_safe=False)
