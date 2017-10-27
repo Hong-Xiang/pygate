0c1f8c29dccb3a72750c430c6794aa9b59650a2b
HongXiang <hx.hongxiang@gmail.com>
2017-10-27 21:25:19 +0800

    fix manifest

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='dxl-pygate',
-      version='0.5.0',
+      version='0.7.0',
       description='A simplified python interface for Gate.',
       url='https://github.com/Hong-Xiang/pygate',
       author='Hong Xiang',
       author_email='hx.hongxiang@gmail.com',
       license='MIT',
-      packages=['pygate', 'pygate.template'],
-      install_requires=['fs', 'click', 'pyyaml', 'rx', 'jinja2', 'dxl-dxpy>=0.4'],
+      packages=find_packages(),
+      install_requires=['fs', 'click', 'pyyaml',
+                        'rx', 'jinja2', 'dxl-dxpy>=0.4'],
       scripts=['bin/pygate'],
       zip_safe=False)
