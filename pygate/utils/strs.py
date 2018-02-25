fa4e5e0317ec6d690cad9f6f225c3b0e6d52754a
HongXiang <hx.hongxiang@gmail.com>
2018-02-25 17:00:50 +0800

    add period acusition and random engine

--- pygate/utils/strs.py
+++ pygate/utils/strs.py
@@ -1,5 +1,15 @@
 def ignore_empty_newlines(s):
     return '\n'.join([x for x in s.split('\n') if x != ''])
 
+
 def join_multiple(s, to_join):
-    return to_join.join([x for x in s.split(to_join) if x != '']) 
+    return to_join.join([x for x in s.split(to_join) if x != ''])
+
+
+def ignore_multiple_whitespaces(s):
+    return join_multiple(join_multiple(s, ' '), '\n')
+
+
+def assert_equal_ignoring_multiple_whitespaces(obj, first, second):
+    return obj.assertEqual(ignore_multiple_whitespaces(first),
+                           ignore_multiple_whitespaces(second))
