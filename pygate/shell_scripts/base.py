31f6e0c23baf04d3b24ff7a030ceb580e608e616
HongXiang <hx.hongxiang@gmail.com>
2018-02-26 22:34:06 +0800

    clean base to components, add base to shell scripts

--- pygate/shell_scripts/base.py
+++ pygate/shell_scripts/base.py
@@ -1,3 +1,8 @@
-from . import __name__ as pname
-def foo():
-    print(pname)
+from . import __name__ as pkg_name
+from ..utils.object_with_template import EnvironmentOfPackage, ObjectWithTemplateBase
+
+env = EnvironmentOfPackage(pkg_name)
+
+
+class ObjectWithTemplate(ObjectWithTemplateBase):
+    pkg_env = env
