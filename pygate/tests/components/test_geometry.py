0d606b532ba11b3c7ca5149ecdf418de4e20f08c
HongXiang <hx.hongxiang@gmail.com>
2018-02-26 21:45:26 +0800

    add geometry test

--- pygate/tests/components/test_geometry.py
+++ pygate/tests/components/test_geometry.py
@@ -87,6 +87,7 @@ class TestSurface(unittest.TestCase):
         ae(self, s.render(),
            '/gate/crystal/surfaces/name                           Detection1\n/gate/crystal/surfaces/insert                         Electronics\n/gate/crystal/surfaces/Detection1/setSurface          perfect_apd ')
 
+
 class TestPhantom(unittest.TestCase):
     def test_render(self):
         class DummyVolume:
@@ -94,7 +95,6 @@ class TestPhantom(unittest.TestCase):
         p = Phantom((DummyVolume,))
         self.assertEqual(p.render(),
                          '/gate/phantom/attachPhantomSD\n')
-
 
 
 class TestCamera(unittest.TestCase):
@@ -123,6 +123,7 @@ class TestCamera(unittest.TestCase):
         ae(self, camera.render(),
            '/gate/systems/cylindricalPET/rsector/attach head\n/gate/systems/cylindricalPET/module/attach block\n/gate/systems/cylindricalPET/crystal/attach crystal\n/gate/systems/cylindricalPET/layer0/attach LSO\n/gate/systems/cylindricalPET/layer1/attach BGO\n/gate/LSO/attachCrystalSD\n/gate/BGO/attachCrystalSD')
 
+
 class TestCylindrical(unittest.TestCase):
     def test_render(self):
         world = Box('world', Vec3(400.0, 400.0, 400.0, 'cm'))
@@ -141,3 +142,30 @@ class TestCylindrical(unittest.TestCase):
         self.assertEqual(system.render(),
                          '/gate/systems/cylindricalPET/rsector/attach   head\n/gate/systems/cylindricalPET/module/attach   block\n/gate/systems/cylindricalPET/crystal/attach   crystal\n/gate/systems/cylindricalPET/layer0/attach   LSO\n/gate/systems/cylindricalPET/layer1/attach   BGO\n')
 
+
+class TestGeometry(unittest.TestCase):
+    def test_render(self):
+        world = Box('world', Vec3(400.0, 400.0, 400.0, 'cm'))
+        cylinder = Cylinder('cylindricalPET', 520.0, 399.0, 402.0, material='Air',
+                            mother=world, position=Vec3(0.0, 0.0, 0.0, 'cm'), unit='cm')
+        rh = RepeaterRing(4)
+        head = Box('head', Vec3(8, 32, 40, 'cm'), 'Air',
+                   cylinder, Vec3(44.0, 0.0, 0.0, 'cm'), repeaters=[rh])
+        rb = RepeaterCubic(Vec3(1, 20, 20), Vec3(0.0, 1.6, 2.0, 'cm'))
+        block = Box('block', Vec3(30, 16, 20, 'mm'), 'Air', head,
+                    position=Vec3(0.0, 0.0, 0.0, 'cm'), repeaters=[rb])
+        rc = RepeaterCubic(Vec3(1, 5, 5), Vec3(0.0, 3.2, 4.0, 'mm'))
+        crystal = Box('crystal', Vec3(30, 3.0, 3.8, 'mm'), 'Air',
+                      block, position=Vec3(0.0, 0.0, 0.0, 'cm'), repeaters=[rc])
+        lso = Box('LSO', Vec3(15, 3.0, 3.8), 'LSO',
+                  crystal, Vec3(-0.75, 0.0, 0.0, 'cm'))
+        bgo = Box('BGO', Vec3(15, 3.0, 3.8), 'BGO',
+                  crystal, Vec3(0.75, 0.0, 0.0, 'cm'))
+        system = CylindericalPET(head, block, crystal=crystal,
+                                 layer0=lso, layer1=bgo)
+        camera = Camera(system, [lso, bgo])
+        casper = Box('casper', Vec3(50.0, 50.0, 17.0, 'mm'), 'RhB', world)
+        phantom = Phantom((casper,))
+
+        surfaces = (SurfacePerfectAPD('Detection1', block, crystal),)
+        geometry = Geometry(world, camera, phantom, surfaces)
