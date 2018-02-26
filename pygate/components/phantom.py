0907c299cc3ae7d940583437a46f7220bc381d34
HongXiang <hx.hongxiang@gmail.com>
2018-02-26 19:50:32 +0800

    add phantom

--- pygate/components/phantom.py
+++ pygate/components/phantom.py
@@ -6,5 +6,5 @@ from typing import Tuple
 class Phantom(ObjectWithTemplate):
     template = 'phantom'
 
-    def __init__(self, sensitive_detectors: Tuple[Volume]=())
+    def __init__(self, sensitive_detectors: Tuple[Volume]=()):
         self.sds = sensitive_detectors
