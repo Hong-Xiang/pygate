586d2c4f19f531315fbba00b4a399f1827bcdd9e
HongXiang <hx.hongxiang@gmail.com>
2018-03-08 15:59:49 +0800

    add dryrun test OpGenerateFile

--- pygate/tests/routine/test_initialize.py
+++ pygate/tests/routine/test_initialize.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest.mock import patch
 from fs.memoryfs import MemoryFS
-from dxl.fs import Directory
+from dxl.fs import Directory, File
 from pygate.routine.base import RoutineOnDirectory
 from pygate.routine import initialize as ini
 
@@ -24,3 +25,15 @@ class TestSubdirectoriesMaker(unittest.TestCase):
         dirs = ['sub.{}'.format(i) for i in range(3)]
         for d in dirs:
             self.assertTrue(mfs.exists(d))
+
+
+class TestOpGenerateFile(unittest.TestCase):
+    @patch('pygate.routine.initialize.OpGenerateFile.content', return_value='test text')
+    def test_dryrun(self, m):
+        mfs = MemoryFS()
+        d = Directory('.', mfs)
+        o = ini.OpGenerateFile('test.txt')
+        r = RoutineOnDirectory(d, [o])
+        self.assertEqual(o.dryrun(r),
+                         ini.TargetFileWithContent(File('test.txt', mfs),
+                                                   content='test text').to_dict())
