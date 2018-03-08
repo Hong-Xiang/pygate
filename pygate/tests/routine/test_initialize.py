75c45a5536b7ac49528448f9e9e4d2590caf75ba
HongXiang <hx.hongxiang@gmail.com>
2018-03-08 15:52:38 +0800

    add SubdirectoriesMaker apply

--- pygate/tests/routine/test_initialize.py
+++ pygate/tests/routine/test_initialize.py
@@ -14,3 +14,13 @@ class TestSubdirectoriesMaker(unittest.TestCase):
         result = o.dryrun(r)
         self.assertEqual(result[ini.KEYS.SUBDIRECTORIES],
                          ('sub.0', 'sub.1', 'sub.2'))
+
+    def test_apply(self):
+        mfs = MemoryFS()
+        d = Directory('.', mfs)
+        o = ini.OpSubdirectoriesMaker(3)
+        r = RoutineOnDirectory(d, [o], dryrun=True)
+        result = o.apply(r)
+        dirs = ['sub.{}'.format(i) for i in range(3)]
+        for d in dirs:
+            self.assertTrue(mfs.exists(d))
