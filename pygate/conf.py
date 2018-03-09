385c25e92ceb801fb8ff0f521fc5d4ee778b787b
HongXiang <hx.hongxiang@gmail.com>
2018-03-09 14:20:39 +0800

    clean -s CLI ok

--- pygate/conf.py
+++ pygate/conf.py
@@ -4,11 +4,18 @@ class KEYS:
     SUB_PREFIX = 'sub_prefix'
     SUB_PATTERNS = 'sub_patterns'
     SUB_FORMAT = 'sub_format'
+    CLEAN = 'clean'
 
 
 class SUBMIT_KEYS:
     BROADCAST = 'broadcast'
     SINGLE = 'single'
+
+
+class CLEAN_KEYS:
+    IS_SUBDIRECTORIES = 'is_subdirectories'
+    ROOT_FILES = 'root_files'
+    IS_SLURM_OUTPUTS = 'is_slurm_outputs'
 
 
 config = {
@@ -17,5 +24,10 @@ config = {
         SUBMIT_KEYS.SINGLE: ['post.sh'],
     },
     KEYS.DRYRUN: False,
-    KEYS.SUB_PREFIX: 'sub'
+    KEYS.SUB_PREFIX: 'sub',
+    KEYS.CLEAN: {
+        CLEAN_KEYS.IS_SUBDIRECTORIES: True,
+        CLEAN_KEYS.IS_SLURM_OUTPUTS: False,
+        CLEAN_KEYS.ROOT_FILES: (),
+    }
 }
