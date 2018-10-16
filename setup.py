17fea4e79535cdc69e3c64d6999d72a0cde160b6
hongjiang <hongjiangdong@outlook.com>
2018-10-16 17:22:37 +0800

    add install requires

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(name='dxl-pygate',
-      version='0.13.1',
+      version='0.13.2',
       description='A simplified python interface for Gate.',
       url='https://github.com/tech-pi/pygate',
       author='Hong Xiang',
       author_email='hx.hongxiang@gmail.com',
       license='MIT',
       packages=find_packages(),
-      install_requires=['fs', 'click',
-                        'rx', 'jinja2',
-                        'jfs', 'dxl-cluster>=0.0.5', 'dask'],
+      install_requires=[
+                        'dxl-cluster=0.0.6',
+                        'dxl-shape=0.1.2'
+                        'dxl-fs=0.1.5',
+                        'typing',
+                        'fs',
+                        'click',
+                        'rx',
+                        'jinja2',
+                        'jfs',
+                        'dask',
+                        'pandas',
+                        'pathlib',
+                        'numpy',
+                        'scipy',
+      ],
       entry_points='''
             [console_scripts]
             pygate=pygate.cli:cli
